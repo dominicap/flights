23a34d9800867511df1e4275429ef75da207554e
Dominic Philip <domi.a.philip@gmail.com>
2020-02-07 12:26:44 -0500

    add designs, README and LICENSE

---
+++ Flights/View/FlightCard.swift
@@ -0,0 +1,21 @@
+//
+//  FlightCard.swift
+//  Flights
+//
+//  Created by Dominic Philip on 2/6/20.
+//  Copyright © 2020 Dominic Philip. All rights reserved.
+//
+
+import SwiftUI
+
+struct FlightCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FlightCard_Previews: PreviewProvider {
+    static var previews: some View {
+        FlightCard()
+    }
+}
